6a35de60ca7292e80a4dea71d7d4d815d1528253
Ishika-1208 <guptaishika1208@gmail.com>
2026-07-02 15:14:16 +0530

    Use environment variable

--- src/services/WeatherAPI.js
+++ src/services/WeatherAPI.js
@@ -1,4 +1,4 @@
-const API_KEY ="812ed7e82c584531dda980715d3fa6d6";
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 const BASE_URL ="https://api.openweathermap.org/data/2.5";
 const GEO_URL ="https://api.openweathermap.org/geo/1.0";
 
